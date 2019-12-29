61954a0b4f4ba0450546372dd089ebe0914a4761
Ash0218 <ajykim218@gmail.com>
2019-12-28 23:42:28 -0500

    Added runner class and step definitions

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -21,4 +21,20 @@ public class LoginStepDefinitions {
     public void user_verifies_that_page_subtitle_is_displayed(String string) {
         System.out.println("Verifying page subtitle: " + string);
     }
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        System.out.println("I am on the login page");
+    }
+
+    @Then("user logs in as driver")
+    public void user_logs_in_as_driver() {
+        System.out.println("Login as a driver");
+    }
+
+    @Then("user logs in as sales manager")
+    public void user_logs_in_as_sales_manager() {
+        System.out.println("Login as a sales manager");
+    }
+
 }
