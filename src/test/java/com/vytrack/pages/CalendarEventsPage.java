1c6f3b1631a9ea72051803200a4348902cfb20ca
Ash0218 <ajykim218@gmail.com>
2020-01-14 11:59:18 -0500

    added new method for converting list of webEements into list of Strings

--- src/test/java/com/vytrack/pages/CalendarEventsPage.java
+++ src/test/java/com/vytrack/pages/CalendarEventsPage.java
@@ -4,12 +4,17 @@ import com.vytrack.utilities.BrowserUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 
 public class CalendarEventsPage extends BasePage{ // 1
     // add extends BasePage
 
     @FindBy(css = "[title='Create Calendar event']") // 3
     public WebElement createCalendarEvent; // 2
+
+    @FindBy(css = "span[class='grid-header-cell__label']") // 9
+    public List<WebElement> columnNames; // 8
 
     public void clickToCreateCalendarEvent(){ // 4
         BrowserUtils.waitForVisibility(createCalendarEvent, 5); // 6
