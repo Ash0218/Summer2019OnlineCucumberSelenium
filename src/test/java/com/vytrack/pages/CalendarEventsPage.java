3d3fe1fe4d60354d0983a331dde640ff3ae242d6
Ash0218 <ajykim218@gmail.com>
2020-01-14 12:19:05 -0500

    added new method for converting list of webEements into list of Strings

--- src/test/java/com/vytrack/pages/CalendarEventsPage.java
+++ src/test/java/com/vytrack/pages/CalendarEventsPage.java
@@ -16,6 +16,12 @@ public class CalendarEventsPage extends BasePage{ // 1
     @FindBy(css = "span[class='grid-header-cell__label']") // 9
     public List<WebElement> columnNames; // 8
 
+    @FindBy(css = "button[class*='btn dropdown-toggle']") // 13
+    public WebElement viewPerPageToggle; // 12
+
+    @FindBy(css = "[class*='btn-group'] [class='dropdown-menu pull-right'] li") // 15
+    public List<WebElement> viewPerPageOptions; // 14
+
     public void clickToCreateCalendarEvent(){ // 4
         BrowserUtils.waitForVisibility(createCalendarEvent, 5); // 6
         BrowserUtils.waitForClickablility(createCalendarEvent, 5); // 7
@@ -24,5 +30,12 @@ public class CalendarEventsPage extends BasePage{ // 1
 
     public List<String> getColumnNames(){ // 10
         return BrowserUtils.getListOfString(columnNames); // 11
+    }
+
+    public List<String> getViewPerPageOptions(){ // 16
+        BrowserUtils.waitForVisibility(viewPerPageToggle, 10); // 17
+        BrowserUtils.clickWithWait(viewPerPageToggle); // 18
+        return BrowserUtils.getListOfString(viewPerPageOptions); // 19
+
     }
 }
